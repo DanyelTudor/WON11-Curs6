470f47ea5cedeb4e527b3852cbac37283a9ffc9e
Daniel <marchisdaniel31@gmail.com>
2023-04-11 02:45:25 +0300

    final lab 6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is an experiment.");
         System.out.println("Hello world!");
+        Lamp lamp1 = new Lamp();
+        Lamp lamp2 = new Lamp();
+
+        lamp1.turnOn();
+        lamp2.turnOn();
+
+        Lamp customLamp = new Lamp("green", 10);
+        customLamp.turnOn();
+        customLamp.increaseIntensity();
+        customLamp.turnOff();
+        customLamp.turnOn();
+
+        Random random = new Random();
+        random.nextInt();
     }
 }
